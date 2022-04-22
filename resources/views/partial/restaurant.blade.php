32305d6a65b4c7be0e4444604dcfe08781e18b8b
noopy0415 <noopy0415@gmail.com>
2022-04-22 16:09:09 +0900

    fix

--- resources/views/partial/restaurant.blade.php
+++ resources/views/partial/restaurant.blade.php
@@ -1,11 +1,11 @@
-<div class="row">
+<div class="row position-relative">
     <div>
         <img src="{{ Storage::disk('admin')->url($restaurant->img_path) }}" class="square-img">
     </div>
     <div class="ml-3">
         <div class="mt-3 mb-3">
             <h3>
-                <a href="{{ route('restaurants.show', $restaurant->id) }}">{{ $restaurant->name }}</a>
+                <a href="{{ route('restaurants.show', $restaurant->id) }}" class="stretched-link">{{ $restaurant->name }}</a>
             </h3>
         </div>
         <div>
@@ -14,7 +14,7 @@
             @if (!empty($restaurant->menus))
                 <ul class="menu_images">
                     @foreach ($restaurant->menus as $menu)
-                        <li class="item"><img src="{{ $menu->img_path }}" class="square-img-s"></li>
+                        <li class="item"><img src="{{ asset($menu->img_path) }}" class="square-img-s"></li>
                         @if ($loop->iteration>=7)
                             @break
                         @endif
